7b885a239bf7d91e33cad21a3317c4360cdd9b57
Steven Cao <anhhung_so11@yahoo.com>
2023-03-15 16:34:37 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column" }} className="App">
+      <img
+        alt=""
+        src="https://media.gettyimages.com/id/167866478/photo/strawberry.jpg?s=612x612&w=gi&k=20&c=hP-3tsdT85B3-AHQ8n1r-9IsdLDKNLujVd9xeHeKuqQ="
+      />
+      <img
+        alt=""
+        src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?cs=srgb&dl=pexels-pixabay-326055.jpg&fm=jpg"
+      />
+      <img
+        alt=""
+        src="https://us.123rf.com/450wm/maaravic/maaravic2210/maaravic221000286/193002567-background-of-butterflies-of-different-colors-rainbow-different-sizes-and-shapes-very-beautiful.jpg?ver=6"
+      />
+      <img
+        alt=""
+        src="https://thumbs.dreamstime.com/b/d-mural-wallpaper-beautiful-view-landscape-background-old-arches-tree-sun-water-birds-flowers-transparent-curtains-166191190.jpg"
+      />
     </div>
   );
 }
